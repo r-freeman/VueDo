f7e03d23172776b5f51b04a1986d182089d4c6ae
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-14 15:53:49 +0000

    Added TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-list-group-item>
+            {{ todo.text }}
+        </b-list-group-item>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: ['todo']
+    }
+</script>
+
+<style scoped>
+
+</style>
