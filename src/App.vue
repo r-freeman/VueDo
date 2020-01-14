d319410610ce29da501690c3416b617fcb281859
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-14 14:46:31 +0000

    Added TodoList container component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="app">
-
-  </div>
+    <div id="app">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <TodoList/>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
+    import TodoList from "./components/TodoList";
 
-export default {
-  name: 'app',
-  components: {}
-}
+    export default {
+        name: 'app',
+        components: {TodoList}
+    }
 </script>
 
 <style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-card header="TodoList" header-tag="header">
+            <template v-slot:footer>
+                <b-button variant="success" class="float-right">Add Todo</b-button>
+            </template>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoList"
+    }
+</script>
+
+<style scoped>
+
+</style>
